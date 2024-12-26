207f13b8ee8bcdffb7e58e5709eabc083a4efaf3
MANEESHCHITI <maneeshchiti@gmail.com>
2024-12-26 12:58:38 +0530

    Delete ChatBotProject directory

--- ChatBotProject/src/main/java/com/UpwardIq/ChatBotProject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.UpwardIq.ChatBotProject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.UpwardIq.ChatBotProject.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
-}
